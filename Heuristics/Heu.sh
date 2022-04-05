2056c9381f3595db177bc13619953b5d457c7de7
Sijing Tu <sijing@kth.se>
2022-04-05 15:38:31 +0200

    update

--- Heuristics/Heu.sh
+++ Heuristics/Heu.sh
@@ -3,7 +3,7 @@
 for model in 1 ; do
     for init_assign in 2 ; do
         for k in 1 ; do
-            for var in ../data/raw/* ;do
+            for var in ../data/all/* ;do
                 julia -O3 MethodLinear.jl $(basename $var .jld2) $model $init_assign $k 1
             done
         done
